355a28ce28f1a5ce991cb161b52d0aa2a2310e03
Norluis Galvez <norly021196@gmail.com>
2024-09-12 01:36:13 -0400

    responsive hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,20 +2,39 @@ import Button from "./ui/Button";
 
 const Hero = () => {
   return (
-    <section className="grid grid-cols-2 justify-between font-light bg-slate-100  ">
-      <div className=" grid-cols-1 my-auto pl-32 ">
-        <h1 className=" text-dark-blue mr-40 font-PublicSans text-7xl">
+    <section className="grid grid-cols-1 lg:grid-cols-2 lg:items-center justify-between font-light bg-slate-100  ">
+      {/* Design Mobile */}
+      <div className="lg:hidden flex overflow-hidden">
+        <img
+          className=" z-10 w-full"
+          src="/images/bg-intro-mobile.svg"
+          alt=""
+        />
+        <img
+          className="absolute w-full -mt-[100px] z-20"
+          src="/images/image-mockups.png"
+          alt=""
+        />
+      </div>
+
+      {/* Hero */}
+      <div className="lg:pl-32 text-center mx-10 lg:mx-0 lg:my-40">
+        <h1 className="text-dark-blue text-center lg:text-start font-PublicSans text-5xl md:text-6xl">
           Next generation <br />
           digital banking
         </h1>
-        <h2 className="my-7 text-xl text-grayish-blue">
+        <h2 className="my-5 lg:my-7 text-lg lg:text-xl text-center lg:text-start text-grayish-blue">
           Take your financial life online. Your Easybank account <br /> will be
           a one-stop-shop for spending, saving,
           <br /> budgeting, investing, and much more.
         </h2>
-        <Button />
+        <div className="flex justify-center lg:justify-start">
+          <Button />
+        </div>
       </div>
-      <div className=" grid-cols-1 overflow-hidden ">
+
+      {/* Design Desktop */}
+      <div className="hidden lg:block overflow-hidden">
         <img className=" z-10 " src="/images/bg-intro-desktop.svg" alt="" />
         <img
           className="absolute -mt-[780px] translate-x-36 z-20"
